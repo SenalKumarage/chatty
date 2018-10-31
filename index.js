0079e575cd6623f5035ab6e403daf66026f4300f
Senal Kumarage <senalkumarage@gmail.com>
2018-10-31 10:53:09 +0800

    Completing the basic chat

--- index.js
+++ index.js
@@ -1,9 +1,25 @@
 
 let app = require('express')();
 let http = require('http').Server(app);
+let io = require('socket.io')(http);
 
 app.get('/', function (req, res) {
     res.sendFile(__dirname + '/index.html');
+});
+
+io.on('connection', (socket) => {
+
+    console.log('User connected');
+
+    socket.on('chat message', function (msg) {
+
+        io.emit('chat message', msg);
+    });
+
+    socket.on('disconnect', () => {
+
+        console.log('User disconnected');
+    });
 });
 
 http.listen(3000, () => {
